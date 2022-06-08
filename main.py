c95f51442fa0283f768a514a608b717753ca1e62
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-08 22:49:28 +0000

    added an API for automatic quotes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 import discord
+import requests
+import json
 #create an instance of client
 client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+
+  json_data = json.loads(response.text)
+
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
 
 #register an event
 @client.event
@@ -13,8 +23,13 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello dear!')
+  if message.content.startswith('Hello'):
+    await message.channel.send('Hello dear! Let me inspire you today')
+
+  if message.content.startswith('Inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+  
 
 client.run(os.getenv('TOKEN'))
   
