10a8c25735d2f61cb48b10ab54f734bf3393bf54
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-08 22:10:43 +0000

    registered two events

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import os
 import discord
+#create an instance of client
+client = discord.Client()
+
+#register an event
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello dear!')
+
+client.run(os.getenv('TOKEN'))
+  
