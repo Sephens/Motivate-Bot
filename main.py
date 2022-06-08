11fba17476456bdced590a8cc9645a85b98c02eb
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-08 23:14:58 +0000

    Created some random sad words in a users input and random starter motivations from the bot

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import os
 import discord
 import requests
 import json
+import random
 #create an instance of client
 client = discord.Client()
+
+sad_words = ["sad","unhappy","hard","dipressed", "low", "lonely", "angry", "misserable","dipressing", "lost", "lack", "tired"]
+
+starter_motivations = [
+  "Cheer up!","Dont be sad!", "All is gonna be well", "You are going to pass through this!","God loves you remember"
+]
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -22,6 +29,8 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+    
+  msg = message.content
 
   if message.content.startswith('Hello'):
     await message.channel.send('Hello dear! Let me inspire you today')
@@ -29,6 +38,9 @@ async def on_message(message):
   if message.content.startswith('Inspire'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_motivations))
   
 
 client.run(os.getenv('TOKEN'))
