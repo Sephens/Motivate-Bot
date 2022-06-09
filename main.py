800553997ed68fae367cbab33231ff2649aa008d
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-09 12:17:28 +0000

    added ability to turn the bot on or off

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
+#import discord 
 import discord
+#import for http requests from API
 import requests
 import json
 import random
-
-#imports for replit db usage
 from replit import db
 #create an instance of client
 client = discord.Client()
@@ -12,10 +12,16 @@ client = discord.Client()
 sad_words = ["sad","unhappy","hard","dipressed", "low", "lonely", "angry", "misserable","dipressing", "lost", "lack", "tired"]
 
 starter_motivations = [
-  "Cheer up!","Dont be sad!", "All is gonna be well", 
-  "You are going to pass through this!",
-  "God loves you remember"
+  "Cheer up!","Dont be sad!", "All is gonna be well", "You are going to pass through this!","God loves you remember"
 ]
+
+song_groups = ["RNB","Gospel","HIPHOP", "Reggea" , "Instrumental"]
+songs = ["Love me now -John Legend","I wish I knew -Steve Brukes","Bwana ni mwokozi -Jeniffer", "Prisoners of guilt -Lucky Dube"]
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -24,26 +30,6 @@ def get_quote():
 
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
-
-def update_motivations(motivating_nessages):
-  if "motivations" in db.keys():
-    motivations = db["motivations"]
-    motivations.append(motivating_nessages)
-    #save to database
-    db["motivations"] = motivations
-
-  else:
-    db["motivations"] = [motivating_nessages]
-
-
-#function to delete a motivating message from the db
-def delete_motivation(index):
-  #get the list of motivations from the db
-  motivations = db["motivations"]
-  if len(motivations) > index:
-    del motivations[index]
-    #save into the db again
-    db["motivations"] = motivations
 
 #register an event
 @client.event
@@ -56,35 +42,34 @@ async def on_message(message):
     return
     
   msg = message.content
-
-  if message.content.startswith('Hello'):
-    await message.channel.send('Hello dear! Let me inspire you today')
-
-  if message.content.startswith('Inspire'):
-    quote = get_quote()
-    await message.channel.send(quote)
-
-  options = starter_motivations
-  if "motivations" in db.keys():
-    options = options + db["motivations"]
-
-
-  if any(word in msg for word in sad_words):
-    await message.channel.send(random.choice(options))
-
-  if msg.startswith("$new"):
-    motivating_message = msg.split("$new ",1)[1]
-    update_motivations(motivating_message)
-    await message.channel.send("New motivating message added")
-
-  if msg.startswith("$del"):
-    motivations = []
-    if "motivations" in db.keys():
-      index = int(msg.split("$del",1)[1])
-      delete_motivation(index)
-      motivations = db["motivations"]
-    await message.channel.send(motivations)
-      
+  if db["responding"]:
+      if message.content.startswith('Hello'):
+        await message.channel.send('Hello dear! Tell me anything')
     
+      if message.content.startswith('Sing for me'):
+        await message.channel.send('Which is your fav group?')
+    
+      if any(word in msg for word in song_groups):
+        await message.channel.send(random.choice(songs))
+    
+      if message.content.startswith('Inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+    
+      if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_motivations))
+
+  #turn the bot on or off
+  if msg.startswith("Responding"):
+    value = msg.split("Responding ",1)[1]
+    if value.lower() == ("true"):
+      db["responding"] = True
+      await message.channel.send("Response turned on")
+    elif value.lower() == ("false"):
+      db["responding"] = False
+      await message.channel.send("Response is turned off")
+
+  
+
 client.run(os.getenv('TOKEN'))
   
